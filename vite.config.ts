8b5f865202d12d9a342b0d909b32d2da262c289e
Leonardo Manuel Alvarez <35227951+alvarez25leo@users.noreply.github.com>
2024-04-27 20:52:35 -0500

    Actualizar configuración de Vite y dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import wasm from 'vite-plugin-wasm';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { resolve } from "path"
+import wasm from "vite-plugin-wasm"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    drop: process.env.NODE_ENV !== "development" ? ["console", "debugger"] : [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "~": resolve(__dirname, "node_modules"),
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  plugins: [react(), wasm()],
-});
+	build: {
+		target: "esnext",
+	},
+	esbuild: {
+		drop: process.env.NODE_ENV !== "development" ? ["console", "debugger"] : [],
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: "globalThis",
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"~": resolve(__dirname, "node_modules"),
+			"@": resolve(__dirname, "src"),
+		},
+	},
+	plugins: [react(), wasm()],
+})
